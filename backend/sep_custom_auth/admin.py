58268878a7ac708cde54784650d1e3af346d872a
Mathieu-COSYNS <64072917+Mathieu-COSYNS@users.noreply.github.com>
2021-07-07 01:11:36 +0200

    auth: replace form with defaults from

--- backend/sep_custom_auth/admin.py
+++ backend/sep_custom_auth/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .forms import UserCreateForm, UserUpdateForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 User = get_user_model()
 
@@ -11,8 +11,8 @@ admin.site.unregister(Group)
 
 class UserAdmin(BaseUserAdmin):
     # The forms to add and change user instances
-    form = UserUpdateForm
-    add_form = UserCreateForm
+    form = UserChangeForm
+    add_form = UserCreationForm
 
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
@@ -28,9 +28,8 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('name', 'password', 'confirm_password')}
+            'fields': ('name', 'password1', 'password2')}
          ),
-        ('Permissions', {'fields': ('admin',)}),
     )
     search_fields = ['name']
     ordering = ['name']
