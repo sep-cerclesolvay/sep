070dbd73ec8d4c32b7d8818b9d75e4edbc20f0c9
Mathieu-COSYNS <64072917+Mathieu-COSYNS@users.noreply.github.com>
2021-08-07 00:04:45 +0200

    change /docs name

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     path('docs/', TemplateView.as_view(
         template_name='swagger-ui.html',
         extra_context={'schema_url': 'openapi-schema'}
-    ), name='redoc'),
+    ), name='swagger-ui'),
 ]
