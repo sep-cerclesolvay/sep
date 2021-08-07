4663cf50e787809409892d0da0df6aa41e3be44c
Mathieu-COSYNS <64072917+Mathieu-COSYNS@users.noreply.github.com>
2021-08-07 03:09:56 +0200

    add /auth/current-user endpoint

--- backend/sep_custom_auth/urls.py
+++ backend/sep_custom_auth/urls.py
@@ -1,8 +1,9 @@
 from django.urls.conf import path
 
-from sep_custom_auth.views import CustomAuthToken
+from sep_custom_auth.views import CustomAuthToken, CurrentUser
 
 
 urlpatterns = [
-    path('login', CustomAuthToken.as_view())
+    path('login', CustomAuthToken.as_view()),
+    path('current-user', CurrentUser.as_view())
 ]
--- backend/sep_custom_auth/views.py
+++ backend/sep_custom_auth/views.py
@@ -1,11 +1,32 @@
+from rest_framework import status
 from rest_framework.parsers import JSONParser
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.throttling import AnonRateThrottle
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
+from sep_custom_auth.models import User
+
+from .serializers import UserSerializer
 
 
 class CustomAuthToken(ObtainAuthToken):
     authentication_classes = []
     throttle_classes = [AnonRateThrottle]
     parser_classes = (JSONParser,)
+
+
+class CurrentUser(APIView):
+    """
+    Retrieve current logged in user
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        try:
+            serializer = UserSerializer(request.user)
+            return Response(serializer.data)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
