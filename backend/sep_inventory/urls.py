ade694cc77691293d17240279d9a32ada5d27f09
Mathieu-COSYNS <64072917+Mathieu-COSYNS@users.noreply.github.com>
2021-08-30 23:27:46 +0200

    add entries & sales  GET endpoints

--- backend/sep_inventory/urls.py
+++ backend/sep_inventory/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include
 from django.urls.conf import path
 from rest_framework import routers
 
-from .views import PackViewSet, PaymentMethodViewSet, ProductViewSet, ReadOnlyPackViewSet
+from .views import PackViewSet, PaymentMethodViewSet, ProductViewSet, ReadOnlyEntryViewSet, ReadOnlyPackViewSet, ReadOnlySaleViewSet
 
 
 class Router(routers.DefaultRouter):
@@ -13,6 +13,8 @@ router = Router()
 router.register('products', ProductViewSet)
 router.register('packs', ReadOnlyPackViewSet)
 router.register('packs', PackViewSet)
+router.register('entries', ReadOnlyEntryViewSet)
+router.register('sales', ReadOnlySaleViewSet)
 router.register('payement-methods', PaymentMethodViewSet)
 
 urlpatterns = [
--- backend/sep_inventory/views.py
+++ backend/sep_inventory/views.py
@@ -1,8 +1,8 @@
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from .serializers import PackSerializer, PaymentMethodSerializer, ProductSerializer, ReadOnlyPackSerializer
-from .models import Pack, PaymentMethod, Product
+from .serializers import PackSerializer, PaymentMethodSerializer, ProductSerializer, ReadOnlyEntrySerializer, ReadOnlyPackSerializer, ReadOnlySaleSerializer
+from .models import Entry, Pack, PaymentMethod, Product, Sale
 
 
 class ProductViewSet(viewsets.ModelViewSet):
@@ -23,6 +23,19 @@ class PackViewSet(mixins.CreateModelMixin,
                   viewsets.GenericViewSet):
     queryset = Pack.objects.all()
     serializer_class = PackSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class ReadOnlyEntryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Entry.objects.filter(deleted_date__isnull=True)
+    serializer_class = ReadOnlyEntrySerializer
+    permission_classes = [IsAuthenticated]
+
+
+class ReadOnlySaleViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Sale.objects.filter(
+        deleted_date__isnull=True).prefetch_related('items', 'sale_to_product')
+    serializer_class = ReadOnlySaleSerializer
     permission_classes = [IsAuthenticated]
 
 
