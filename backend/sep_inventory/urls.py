d95a4dd62c3bf30e2ccb7da53c9fdccd49ecb1a4
Mathieu-COSYNS <64072917+Mathieu-COSYNS@users.noreply.github.com>
2021-10-29 17:20:09 +0200

    Add sales report in the backend

--- backend/sep_inventory/urls.py
+++ backend/sep_inventory/urls.py
@@ -2,7 +2,12 @@ from django.conf.urls import include
 from django.urls.conf import path
 from rest_framework import routers
 
-from .views import PackViewSet, PaymentMethodViewSet, ProductViewSet, EntryViewSet, SaleViewSet
+from .views import PackViewSet
+from .views import PaymentMethodViewSet
+from .views import ProductViewSet
+from .views import EntryViewSet
+from .views import SaleViewSet
+from .views import get_sale_report
 
 
 class Router(routers.DefaultRouter):
@@ -17,5 +22,6 @@ router.register('sales', SaleViewSet)
 router.register('payment-methods', PaymentMethodViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('reports/sales', get_sale_report),
+    path('', include(router.urls))
 ]
--- backend/sep_inventory/views.py
+++ backend/sep_inventory/views.py
@@ -1,9 +1,23 @@
+import pandas as pd
+import numpy as np
+
+from django.db.models import Sum
+from django.db.models.functions import TruncDate
+
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from sep_inventory.ReadWriteSerializerMixin import ReadWriteSerializerViewSet
+from sep_inventory.utils import excel_response_from_df, now
 
-from .serializers import ProductSerializer, PaymentMethodSerializer, ReadEntrySerializer, ReadSaleSerializer, ReadPackSerializer, WriteEntrySerializer, WriteSaleSerializer, WritePackSerializer
+from .serializers import ProductSerializer
+from .serializers import PaymentMethodSerializer
+from .serializers import ReadEntrySerializer
+from .serializers import ReadSaleSerializer
+from .serializers import ReadPackSerializer
+from .serializers import WriteEntrySerializer
+from .serializers import WriteSaleSerializer
+from .serializers import WritePackSerializer
 from .models import Entry, Pack, PaymentMethod, Product, Sale
 
 
@@ -39,3 +53,34 @@ class SaleViewSet(ReadWriteSerializerViewSet):
     read_serializer_class = ReadSaleSerializer
     write_serializer_class = WriteSaleSerializer
     permission_classes = [IsAuthenticated]
+
+
+def get_sale_report(request):
+    products = Product.objects.all()
+    sales = Sale.objects.filter(deleted_date__isnull=True)
+    product_df = pd.DataFrame.from_records(
+        products.values('id', 'name', 'buy_price', 'sell_price')
+    )
+    sales_df = pd.DataFrame.from_records(
+        sales.values('items')
+        .annotate(created_date=TruncDate('created_date'))
+        .annotate(Sum('sale_to_product__quantity'))
+    )
+
+    sales_df = sales_df.pivot(index='items', columns='created_date')
+    sales_df.columns = sales_df.columns.droplevel(0)
+    sales_df = sales_df.reset_index()
+    sales_df.rename(columns={'items': 'id'}, inplace=True)
+    sales_df.fillna(0, inplace=True)
+
+    df = pd.merge(product_df, sales_df)
+    df = df.T.set_index(np.concatenate((np.repeat('Products', 4),
+                        np.repeat('Sales', df.shape[1] - 4))), append=True).T
+    df = df.swaplevel(0, 1, 1)
+
+    def worksheet_callback(ws):
+        ws.freeze_panes = 'F3'
+
+    return excel_response_from_df(df, f'{now()}_reports_sales.xlsx', 'reports_sales',
+                                  multi_index=True,
+                                  worksheet_callback=worksheet_callback)
