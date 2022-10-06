aca6140196c9cc914345bd1fe8c617c068b611a9
Mathieu-COSYNS <64072917+Mathieu-COSYNS@users.noreply.github.com>
2022-10-07 01:05:50 +0200

    Attempt Fix: assets issue with favicons

--- frontend/.cracorc.js
+++ frontend/.cracorc.js
@@ -1,54 +1,61 @@
-const WebpackFavicons = require('webpack-favicons');
-
-const production = process.env.NODE_ENV == 'production';
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   webpack: {
     plugins: [
-      new WebpackFavicons(
+      new FaviconsWebpackPlugin(
         {
-          src: './public/assets/logo.svg',
-          path: '/assets/favicons/',
-          appName: 'Solvay - Entraide & Publication',
-          appShortName: 'SEP',
-          appDescription: 'Une application web pour aider à la gestion des ventes du SEP.',
-          developerName: 'Mathieu COSYNS',
-          developerURL: 'https://github.com/Mathieu-COSYNS/',
-          lang: 'fr',
-          background: '#f07e38',
-          theme_color: '#3880ff',
-          appleStatusBarStyle: 'black',
-          display: 'standalone',
-          orientation: 'portrait',
-          scope: '/',
-          start_url: '/',
-          version: process.env.REACT_APP_VERSION,
-          logging: true,
-          icons: {
-            android: production,
-            appleIcon: production,
-            appleStartup: production,
-            favicons: production,
-            windows: production,
-            yandex: production,
+          logo: './public/assets/logo.svg',
+          manifest: {
+            // TODO: fix
+            screenshots: [
+              {
+                src: '/assets/screenshots/screenshot_login.png',
+                type: 'image/png',
+                sizes: '440x804',
+              },
+              {
+                src: '/assets/screenshots/screenshot_menu.png',
+                type: 'image/png',
+                sizes: '440x804',
+              },
+              {
+                src: '/assets/screenshots/screenshot_sales.png',
+                type: 'image/png',
+                sizes: '440x804',
+              },
+            ],
+            url_handlers: [
+              {
+                origin: process.env.REACT_APP_QR_CODE_URL,
+              },
+            ],
           },
-          screenshots: [
-            {
-              src: '/assets/screenshots/screenshot_login.png',
-              type: 'image/png',
-              sizes: '440x804',
+          favicons: {
+            appName: 'Solvay - Entraide & Publication',
+            appShortName: 'SEP',
+            appDescription: 'Une application web pour aider à la gestion des ventes du SEP.',
+            developerName: 'Mathieu COSYNS',
+            developerURL: 'https://github.com/Mathieu-COSYNS/',
+            lang: 'fr',
+            background: '#f07e38',
+            theme_color: '#3880ff',
+            appleStatusBarStyle: 'black',
+            display: 'standalone',
+            orientation: 'portrait',
+            scope: '/',
+            start_url: '/',
+            version: process.env.REACT_APP_VERSION,
+            logging: true,
+            icons: {
+              android: true,
+              appleIcon: true,
+              appleStartup: true,
+              favicons: true,
+              windows: true,
+              yandex: true,
             },
-            {
-              src: '/assets/screenshots/screenshot_menu.png',
-              type: 'image/png',
-              sizes: '440x804',
-            },
-            {
-              src: '/assets/screenshots/screenshot_sales.png',
-              type: 'image/png',
-              sizes: '440x804',
-            },
-          ],
+          },
         },
         (response) => {
           for (file of response.files) {
